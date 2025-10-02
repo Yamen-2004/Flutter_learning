61e182d0d504c0246287e00d35cd168cddbdcbfc
Yamen Khazar <155854770+Yamen-2004@users.noreply.github.com>
2025-10-02 19:49:45 +0300

    this is session 13

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/screen2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +11,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Stack(
-            children: [
-              Container(width: 200, height: 200, color: Colors.blue),
-              Container(width: 150, height: 150, color: Colors.red),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                left: 150,
-                top: 0,
-                child: Container(width: 50, height: 50, color: Colors.yellow),
+      routes: {
+        "/": (context) => HomePage(),
+        "/screen2": (context) => screen2(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController phone = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('text field'), backgroundColor: Colors.green),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Center(child: Text('Welcome to the Home Page!')),
+
+            Container(
+              width: 300,
+              child: TextField(
+                controller: phone,
+                onChanged: (value) => {
+                    setState(() {})
+                },
+
+                textAlign: TextAlign.end,
+                maxLines: 1,
+
+                decoration: InputDecoration(
+                  fillColor: Colors.red[100],
+                  filled: true,
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  hintText: 'Enter your name',
+                  hintStyle: TextStyle(color: Colors.red),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+
+            Center(child: Text(phone.text)),
+
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+                print(phone.text);
+              },
+              child: Text('Submit'),
+            ),
+          ],
         ),
       ),
     );
